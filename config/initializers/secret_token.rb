a8c5f30df69d91999a33c940a484451713ad5596
PedroBJS <pedro.jancachagua@gmail.com>
2011-11-10 21:50:04 -0500

    Preguna 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TdewPc1::Application.config.secret_token = '39be6f9e962f8c034224ac4267be25d53cf1e9db8ed6921520801f6451fe462b3f491cebdeae6b1018b8e4e7b0bf07d1e073fe816063ea24d3fc8241815e8b21'
