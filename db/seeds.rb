e894395ae2ee748171df3c2823b85868d1e1ec5b
PedroBJS <pedro.jancachagua@gmail.com>
2011-11-10 22:06:38 -0500

    Pregunta 5

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+book_mark.create(:title => "El hablador 1", :link=>"http://www.abc.com",:description => "Libro de MVLL")
+book_mark.create(:title => "El hablador 2", :link=>"http://www.abc.com",:description => "Libro de MVLL")
+book_mark.create(:title => "El hablador 3", :link=>"http://www.abc.com",:description => "Libro de MVLL")
+
+
+# This file shbould contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
